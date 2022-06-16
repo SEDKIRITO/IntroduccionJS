717988dabb644115ac7de89b493c0f92f47661d5
Salvador <salvadored15.com@gmail.com>
2022-06-16 17:04:01 -0500

    first commit

---
+++ IntroduccionJS/js/02.js
@@ -0,0 +1,23 @@
+// variables con let
+
+var producto = 'Audifonos Gamer'; //iniciar variable y darle un valor
+
+var disponible; //iniciamos la variable sin valor 
+
+producto = true; //reasignando el valor de la variable
+
+var producto1 = "computadora",
+    disponoble1 = true,
+    categoria = "computadoras";
+
+// var 1diponible;
+// var -disponoble;
+var _disponoble;
+
+//ESTILOS PARA LAS VARIABLES
+var nombre_producto = "Monitor HD+"; // underscore
+var nombreProducto = "Monitor HD+"; // camel case
+var NombreProducto = "Monitor HD+"; // pascal case
+var nombreproducto = "Monitor HD+"; // lower case
+
+console.log(nombreProducto);
